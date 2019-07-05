f98d18f0da8648db5c26c4a9979354b76e96b5c7
mpepes <piotrmichalek89@gmail.com>
2019-07-05 08:18:03 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  // root: true,
+  // extends: '@react-native-community',
+  "extends": [
+    "@react-native-community",
+    "prettier",
+    "prettier/react"
+  ],
+  "rules": {
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        "trailingComma": "es5",
+        "singleQuote": true,
+        "printWidth": 100
+      }
+    ]
+  },
+  "plugins": [
+    "prettier"
+  ]
+};
---
+++ App.tsx
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+   <Text style={styles.scrollView}>tessssettet</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
